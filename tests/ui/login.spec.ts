ff4d780c01d2628799b866c9677f41c271b1171e
BNMK EH <csemoe@gmail.com>
2026-07-07 13:06:11 +0300

    Add test data management

--- tests/ui/login.spec.ts
+++ tests/ui/login.spec.ts
@@ -1,7 +1,22 @@
 import { test } from '@playwright/test';
 import { LoginPage } from '../../pages/LoginPage';
+import { JsonReader } from '../../utils/JsonReader';
+
+type LoginData = {
+  validUser: {
+    email: string;
+    password: string;
+  };
+  invalidUser: {
+    email: string;
+    password: string;
+  };
+};
+
+const loginData = JsonReader.read<LoginData>('test-data/loginData.json');
 
 test.describe('Login Page Tests', () => {
+
   test('@smoke should display login page successfully', async ({ page }) => {
     const loginPage = new LoginPage(page);
 
@@ -13,7 +28,13 @@ test.describe('Login Page Tests', () => {
     const loginPage = new LoginPage(page);
 
     await loginPage.navigateToLoginPage();
-    await loginPage.login('invalid@test.com', 'wrongPassword');
+
+    await loginPage.login(
+      loginData.invalidUser.email,
+      loginData.invalidUser.password
+    );
+
     await loginPage.verifyInvalidLoginMessage();
   });
+
 });
